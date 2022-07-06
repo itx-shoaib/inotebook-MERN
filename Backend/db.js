337ca1b50045534d223cadd3d4521515fc2685dd
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-06 21:25:59 +0500

    Creating routes and schema

---
+++ Backend/db.js
@@ -0,0 +1,17 @@
+// Storing mongoose in a variable
+const mongoose = require('mongoose');
+
+// Storing DB URI in mongoURI variable
+const mongoURI = "mongodb+srv://shoaib:shoaibjamil43@cluster0.gxfrpaw.mongodb.net/test"
+
+
+// Storing mongoDB connection in a variable.
+const connectToMongo = ()=>{
+    // using mongoose function to connect and giving uri.
+    mongoose.connect(mongoURI, ()=>{
+        // Call back it when it is connected.
+        console.log("MongoDB has been connected successfully");
+    })
+}
+
+module.exports = connectToMongo;
