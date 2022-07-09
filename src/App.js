7c34e06c8369fdfddb464abeabdc01799fcba80a
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-09 23:06:57 +0500

    Context and useContext

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
+    <NoteState>
       <Router>
         <Navbar />
         <Routes>
@@ -18,6 +20,7 @@ function App() {
           </Route>
         </Routes>
       </Router>
+      </NoteState>
     </>
   );
 }
